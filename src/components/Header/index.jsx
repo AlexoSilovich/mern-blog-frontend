524a0af4eedf172747d67aa4259591b67db097e6
Alex <>
2022-07-01 13:52:14 +0300

    fixed bugs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>ALEXO BLOG</div>
+            <div>KVA</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
